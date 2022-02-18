402a6a0f93ffce4b3f2fe1247018d5544e9ed2d8
Miguel Moreno <miguel.moreno@jmdistributions.com>
2022-02-18 16:10:50 -0600

    config file for env file

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+    apikey: process.env.REACT_APP_APIKEY,
+    authdomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectid: process.env.REACT_APP_PROJECTID,
+    storagebucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingsenderid: process.env.REACT_APP_MESSAGINGSENDERID,
+    appid: process.env.REACT_APP_APPID,
+    measurementid: process.env.REACT_APP_MEASUREMENTID,
+}
+
+export default config;
