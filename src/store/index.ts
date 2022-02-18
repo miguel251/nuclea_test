9d2a086cef92b96d8c216741501ad533d51a5891
Miguel Moreno <miguel.moreno@jmdistributions.com>
2022-02-18 16:08:01 -0600

    config for zunstand and immer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,34 @@
 import create from "zustand"
 import produce from "immer";
-import { Category } from "../models/category";
 import { PizzaState } from "../models/PizzaState";
+import { categories, products } from "../__mockup/data";
+import { OrderState } from "../models/OrderState";
+import { Category } from "../models/category";
 
 const useStore = create<PizzaState>((set) => ({
-    Categories: [ {} as Category ],
-    addOrder: (payload: string) =>
+    Categories: [ ...categories ],
+    Product: [ ...products ],
+    Orders: [{}],
+    activeCategory: (id: string) =>
         set(
             produce((draft) => {
+                let categoryByID = draft.Categories.filter((category: Category) => category.id === id);
+                let categoryInactive = draft.Categories.filter((category: Category) => category.active === true);
+                draft.Categories[categoryInactive].active = false;
+                draft.Categories[categoryByID].active = true;
             })
         ),
-    removeOrder: (id: Number) => 
+    addOrder: (payload: OrderState) =>
+        set(
+            produce((draft) => {
+               draft.Orders.push({...payload})
+            })
+        ),
+    removeOrder: (id: string) => 
     set(
         produce((draft) => {
+            let index = draft.Orders.findIndex((order: OrderState) => order.product_id === id);
+            draft.Orders.splice(index, 1);
         })
     ),
 }));
